aca0237919e51fe1f7613dd8daf1cc2b86f7a18c
F4993 <fazlitdin2010@gmail.com>
2026-08-13 04:53:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
 import config
 from handlers import router 
 
-bot = Bot(token=config.BOT_TOKEN)
+# Указываем прокси для PythonAnywhere
+session = AiohttpSession(proxy="http://proxy.server:3128")
+
+bot = Bot(token=config.BOT_TOKEN, session=session)
 dp = Dispatcher()
 
 dp.include_router(router)
