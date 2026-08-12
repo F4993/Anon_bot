89eafecd32134c366935b3512dc3d679acc04d70
F4993 <fazlitdin2010@gmail.com>
2026-08-13 04:57:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.types import BotCommand
 import config
 from handlers import router 
 
-# Указываем прокси для PythonAnywhere
 session = AiohttpSession(proxy="http://proxy.server:3128")
-
 bot = Bot(token=config.BOT_TOKEN, session=session)
 dp = Dispatcher()
 
 dp.include_router(router)
 
 async def main():
-    print("бот запущен ✨")
+    await bot.set_my_commands([
+        BotCommand(command="start", description="Запустить бота")
+    ])
+    print("bot started")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
